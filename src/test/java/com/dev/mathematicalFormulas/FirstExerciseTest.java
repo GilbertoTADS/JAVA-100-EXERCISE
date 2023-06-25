912fecd5dc888d0a7f0018f5476d05f875b05184
GilbertoTADS <gilbertotads@gmail.com>
2023-06-25 11:52:41 -0300

    creation test to sum operation

--- src/test/java/com/dev/mathematicalFormulas/FirstExerciseTest.java
+++ src/test/java/com/dev/mathematicalFormulas/FirstExerciseTest.java
@@ -1,12 +1,15 @@
 package com.dev.mathematicalFormulas;
 
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class FirstExerciseTest {
 
     @Test
     public void sumTwoNumbers(){
-
+        Integer result = ArithMeticalCalc.sum(4,8);
+        assertTrue(result == 12);
     }
     public void subTwoNumbers(){
 
