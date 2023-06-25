8ccd197d64180775b36109c51706b6f953e4d728
GilbertoTADS <gilbertotads@gmail.com>
2023-06-25 16:40:30 -0300

    first exercise done!

--- src/test/java/com/dev/mathematicalFormulas/FirstExerciseTest.java
+++ src/test/java/com/dev/mathematicalFormulas/FirstExerciseTest.java
@@ -4,24 +4,26 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import com.dev.mathematicalFormulas.ArithmeticalCalc;
+
 public class FirstExerciseTest {
 
     @Test
     public void sumTwoNumbers(){
-        Integer result = ArithmeticalCalc.sum(4,8);
+        Double result = ArithmeticalCalc.sum(4.0,8.0);
         assertTrue(result == 12);
     }
     public void subTwoNumbers(){
-        Integer result = ArithmeticalCalc.sub(4,8);
+        Double result = ArithmeticalCalc.sub(4.0,8.8);
         assertTrue(result == 4);
     }
     public void multTwoNumbers(){
-        Integer result = ArithmeticalCalc.mult(4,8);
+        Double result = ArithmeticalCalc.mult(4.0,8.0);
         assertTrue(result == 32);
     }
     public void divTwoNumbers(){
-        Integer result = ArithmeticalCalc.div(4,8);
-        assertTrue(result == 0.5);
+        Double result = ArithmeticalCalc.div(4.0,8.0);
+        assertTrue(result.equals(0.5));
     }
     
 }
