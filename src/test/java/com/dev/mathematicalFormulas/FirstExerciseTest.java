41baed53a478828f020d76133a7d10128c2560d5
GilbertoTADS <gilbertotads@gmail.com>
2023-06-25 11:43:38 -0300

    creation of test cases

---
+++ src/test/java/com/dev/mathematicalFormulas/FirstExerciseTest.java
@@ -0,0 +1,21 @@
+package com.dev.mathematicalFormulas;
+
+import org.junit.Test;
+
+public class FirstExerciseTest {
+
+    @Test
+    public void sumTwoNumbers(){
+
+    }
+    public void subTwoNumbers(){
+
+    }
+    public void multTwoNumbers(){
+
+    }
+    public void divTwoNumbers(){
+
+    }
+    
+}
