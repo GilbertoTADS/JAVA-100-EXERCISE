8f3d272f06aa292b0030d8de94bd5b95f569d584
GilbertoTADS <gilbertotads@gmail.com>
2023-06-25 12:06:49 -0300

    creation of all methods for arithmaticalCalc

--- src/main/java/com/dev/mathematicalFormulas/ArithMeticalCalc.java
+++ src/main/java/com/dev/mathematicalFormulas/ArithMeticalCalc.java
@@ -6,4 +6,16 @@ public class ArithMeticalCalc {
         return i+j;
     }
 
+    public static Integer sub(int i, int j) {
+        return i-j;
+    }
+
+    public static Integer mult(int i, int j) {
+        return i*j;
+    }
+
+    public static Integer div(int i, int j) {
+        return i/j;
+    }
+
 }
