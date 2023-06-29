6536ebc5debcff5fe5552ecc0af11be118f2d2b6
GilbertoTADS <gilbertotads@gmail.com>
2023-06-29 08:34:29 -0300

    implemettation of a method to get circle perimeter

---
+++ src/main/java/com/dev/geometry/GeometryCalc.java
@@ -0,0 +1,12 @@
+package com.dev.geometry;
+
+import com.dev.MathematicalConstants;
+
+public class GeometryCalc {
+
+    public static Double circlePerimeter(double radius) {
+
+        return 2 * MathematicalConstants.PI * radius;
+    }
+
+}
