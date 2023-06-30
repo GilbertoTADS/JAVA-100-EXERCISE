b8088944b716ff311138c0ee27ce9932dcc3c97d
GilbertoTADS <gilbertotads@gmail.com>
2023-06-30 17:52:12 -0300

    refactoring:equation

---
+++ src/main/java/com/dev/mathematicalFormulas/Equation.java
@@ -0,0 +1,9 @@
+package com.dev.mathematicalFormulas;
+
+public class Equation {
+
+    public static Double quadract(double a, double b, double c) {
+        return ((Math.pow(b,2))-4*((a)*(c)));
+    }
+
+}
