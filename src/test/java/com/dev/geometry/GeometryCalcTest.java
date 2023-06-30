e1bc60ca9a37fb61fee15826c3215a1ada381577
GilbertoTADS <gilbertotads@gmail.com>
2023-06-30 17:21:33 -0300

    ninth exercise: test case

--- src/test/java/com/dev/geometry/GeometryCalcTest.java
+++ src/test/java/com/dev/geometry/GeometryCalcTest.java
@@ -16,5 +16,13 @@ public class GeometryCalcTest {
         Double area = GeometryCalc.circleArea(2.0);
         assertEquals((Double)12.566370614359172, area);
     }
+    @Test
+    public void rectangleAreaAndPerimeter(){
+        Double area = GeometryCalc.rectangleArea(12.0,5.0);
+        Double perimeter = GeometryCalc.rectangleArea(2.0,18);
+
+        assertEquals((Double)60.0,area);
+        assertEquals((Double)60.0,perimeter);
+    }
     
 }
