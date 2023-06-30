b0fad4ea00ed5694a75c324c79daed337fdeaddc
GilbertoTADS <gilbertotads@gmail.com>
2023-06-30 18:26:57 -0300

    105º exercise: refactoring

--- src/main/java/com/dev/CollatzCalc.java
+++ src/main/java/com/dev/CollatzCalc.java
@@ -11,12 +11,13 @@ public class CollatzCalc {
     public List<Double> run(Double number) {
         results.add(number);
 
-        if(number.equals((Double)1.0)) return this.results;
-
-        return this.run(this.next(number));
+        return this.isToStop(number) ? this.results : this.run(this.next(number));
     }
     private Double next(Double number){
         return ArithmeticalCalc.isPair(number) ? number/2 : (number*3)+1;
+    }
+    private boolean isToStop(Double number){
+        return number.equals((Double)1.0);
     }
 
 }
